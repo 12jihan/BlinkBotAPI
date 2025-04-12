74cb16d94b3ee8f2e690d64096191a9d2751956f
12jihan <jareem.e.hoff@gmail.com>
2025-04-12 02:22:01 -0400

    update stuff

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express, { Response } from "express";
-import { REST } from '@discordjs/rest';
-import { Routes, Request } from 'discord-api-types/v10';
+import express, { Response, Request } from "express";
+import { RequestData, REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v10';
 import dotenv from "dotenv";
+
 import GuildCommands from "./routes/GuildCommands/GuildManager";
 
 dotenv.config();
@@ -11,17 +12,22 @@ const app = express();
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN!);
 
 
+const msg_obj = (msg: string): RequestData => {
+  return {
+    body:
+    {
+      content: msg
+    }
+  }
+};
+
 app.use("/guild", GuildCommands);
 
-app.get('/test', async (req: Request) => {
+app.get('/test', async (req: Request, res: Response) => {
   try {
-    const disc_response = await rest.post(Routes.channelMessages("1275160714780082209"),
-      {
-        body: {
-          content: "Hi"
-        }
-      }
-    );
+    const disc_msg = await rest.post(Routes.channelMessages("1275160714780082209"), msg_obj("test"));
+    console.log(disc_msg);
+
   } catch (err) {
     console.error(err);
   }
@@ -30,3 +36,5 @@ app.get('/test', async (req: Request) => {
 app.listen(3000, () => {
   console.log("listening on http://localhost:3000/");
 });
+
+
